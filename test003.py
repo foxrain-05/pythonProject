a6f1c5235e5fb41aa680f7fd5aa499b208a195b0
foxrain-05 <1skakaodowx@gmail.com>
2021-10-17 01:02:03 +0900

    크롤링 전화 번호 추가

--- test003.py
+++ test003.py
@@ -19,8 +19,23 @@ for i in range(1, 11, 2):
         continue
     name = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]').text # 이름
     address = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[7]/div[1]/button/div[1]/div[2]/div[1]').text
+    try:
+        phonenum = ""
+        phonenum = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[7]/div[2]/button/div[1]/div[2]/div[1]').text
+    except:
+        try:
+            phonenum = ""
+            phonenum = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[7]/div[4]/button/div[1]/div[2]/div[1]').text
+        except:
+            try:
+                phonenum = ""
+                phonenum = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[7]/div[3]/button/div[1]/div[2]/div[1]').text
+            except:
+                phonenum = ""
+    if ("02-" or "051-" or "053-" or "032-" or "062-" or "052-" or "044-" or "031-" or "033-" or "043-" or "041-" or "061-" or "054-" or "055-" or "064-")not in phonenum:
+            phonenum = ""
     driver.find_element_by_xpath('//*[@id="omnibox-singlebox"]/div[1]/div[1]/button').click()
-    print("이름:" + name + "\n주소:" + address + "\n")
+    print("이름:" + name + "\n주소:" + address + "\n연락처" + phonenum)
 
 driver.quit()
 
